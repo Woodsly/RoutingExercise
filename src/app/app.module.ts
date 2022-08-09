6d28b790d087666213c010d740d1f5de43e29e7a
Connor <Woodconnor13@gmail.com>
2022-08-09 15:50:57 -0400

    Finished base routing exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDetialsComponent } from './movie-detials/movie-detials.component';
+
+const routes: Routes = [
+  {path: 'MovieList', component: MovieListComponent},
+  {path: 'MovieDetails/:id', component: MovieDetialsComponent},
+  {path: '', redirectTo:'/MovieList', pathMatch:'full'},
+  {path: '**', redirectTo:'/MovieList', pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDetialsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/movie.ts
@@ -0,0 +1,7 @@
+export interface Movie {
+    id:number;
+    name:string;
+    rating:number;
+    description:string;
+    image:string;
+}
